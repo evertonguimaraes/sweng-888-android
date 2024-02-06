6a0d6476041957471066ff8848786a86f0e54597
evertonguimaraes <ezt157@psu.edu>
2024-02-06 17:59:34 -0500

    Renaming variables and fixing namespace issues.

---
+++ LessonThreeIntents/app/src/main/java/edu/psu/sweng888/lessonthreeintents/model/CourseListAdapter.java
@@ -0,0 +1,45 @@
+package edu.psu.sweng888.lessonthreeintents.model;
+
+import edu.psu.sweng888.lessonthreeintents.R;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.List;
+
+public class CourseListAdapter extends ArrayAdapter<Course> {
+
+    public CourseListAdapter(@NonNull Context context, @NonNull List<Course> objects) {
+       super(context, R.layout.list_item, objects);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+        View listViewElement = convertView;
+
+        if (listViewElement == null){
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+            listViewElement = inflater.inflate(R.layout.list_item, parent, false);
+        }
+
+        Course course = getItem(position);
+        TextView textViewID = listViewElement.findViewById(R.id.course_id);
+        TextView textViewCredits = listViewElement.findViewById(R.id.course_credits);
+        TextView textViewDescription = listViewElement.findViewById(R.id.course_description);
+
+        textViewID.setText("SWENG: "+course.getID());
+        textViewCredits.setText("Credits: "+course.getCredits());
+        textViewDescription.setText("Description: "+course.getDescription());
+
+        return listViewElement;
+
+    }
+}
